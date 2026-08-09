f4a72152fed6e21bdad0f648b7fb1ee3bb01b9bd
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-09 08:09:26 +0300

    FBD-003: wire Serilog into Generic Host

--- src/FlutterBuildDoctor.App/App.xaml.cs
+++ src/FlutterBuildDoctor.App/App.xaml.cs
@@ -1,7 +1,12 @@
 using System.Windows;
 using FlutterBuildDoctor.App.DependencyInjection;
+using FlutterBuildDoctor.App.Logging;
+using FlutterBuildDoctor.Infrastructure.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace FlutterBuildDoctor.App;
 
@@ -16,11 +21,33 @@ public partial class App : System.Windows.Application
         _host = Host.CreateDefaultBuilder()
             .ConfigureServices(services =>
             {
+                services.AddFlutterBuildDoctorLogging();
                 services.AddFlutterBuildDoctorPresentation();
+            })
+            .UseSerilog((_, services, loggerConfiguration) =>
+            {
+                var inAppSink = services.GetRequiredService<AppLogStoreSink>();
+
+                loggerConfiguration
+                    .MinimumLevel.Debug()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                    .Enrich.FromLogContext()
+                    .Enrich.WithProperty("Application", "FlutterBuildDoctor")
+                    .WriteTo.File(
+                        LoggingPaths.EnsureLogFilePath(),
+                        rollingInterval: RollingInterval.Day,
+                        retainedFileCountLimit: 14,
+                        shared: true,
+                        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                    .WriteTo.Sink(inAppSink);
             })
             .Build();
 
         await _host.StartAsync();
+
+        var logger = _host.Services.GetRequiredService<ILogger<App>>();
+        logger.LogInformation("Flutter Build Doctor host started");
+
         _host.Services.GetRequiredService<MainWindow>().Show();
     }
 
@@ -28,6 +55,7 @@ public partial class App : System.Windows.Application
     {
         if (_host is not null)
         {
+            _host.Services.GetService<ILogger<App>>()?.LogInformation("Flutter Build Doctor host stopping");
             await _host.StopAsync();
             _host.Dispose();
         }
