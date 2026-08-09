49df53831e843a9e8d07a5acc7115221e457d47f
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-09 20:07:10 +0300

    [FBD-608] register Android SDK requirements parser

--- src/FlutterBuildDoctor.App/DependencyInjection/RuntimeDetectionServiceCollectionExtensions.cs
+++ src/FlutterBuildDoctor.App/DependencyInjection/RuntimeDetectionServiceCollectionExtensions.cs
@@ -42,6 +42,7 @@ public static class RuntimeDetectionServiceCollectionExtensions
         services.TryAddSingleton<IGradleWrapperVersionParser, GradleWrapperVersionParser>();
         services.TryAddSingleton<IAndroidGradlePluginVersionParser, AndroidGradlePluginVersionParser>();
         services.TryAddSingleton<IKotlinPluginVersionParser, KotlinPluginVersionParser>();
+        services.TryAddSingleton<IAndroidSdkRequirementsParser, AndroidSdkRequirementsParser>();
         services.TryAddSingleton<IJavaInstallationDetector, JavaInstallationDetector>();
         services.TryAddSingleton<IAndroidSdkRootDetector, AndroidSdkRootDetector>();
         services.TryAddSingleton<IAndroidCommandLineToolsDetector, AndroidCommandLineToolsDetector>();
