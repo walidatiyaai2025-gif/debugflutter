cfa2a5b6200794901e7254d3427d7c3a5e3d9b44
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-09 16:07:39 +0300

    [FBD-416] add immutable environment snapshot contract

---
+++ src/FlutterBuildDoctor.App/Environment/EnvironmentSnapshot.cs
@@ -0,0 +1,31 @@
+using FlutterBuildDoctor.Android.Detection;
+using FlutterBuildDoctor.Application.Environment;
+using FlutterBuildDoctor.Flutter.Detection;
+
+namespace FlutterBuildDoctor.App.Environment;
+
+public sealed record EnvironmentSnapshot(
+    DateTimeOffset CapturedAt,
+    DateTimeOffset CompletedAt,
+    WindowsEnvironmentInfo Windows,
+    EnvironmentVariableSnapshot EnvironmentVariables,
+    FlutterDetectionResult Flutter,
+    DartDetectionResult Dart,
+    JavaDetectionResult Java,
+    AndroidSdkRootDetectionResult AndroidSdk,
+    AndroidCommandLineToolsDetectionResult AndroidCommandLineTools,
+    AndroidAdbDetectionResult Adb,
+    AndroidPlatformDetectionResult AndroidPlatforms,
+    AndroidBuildToolsDetectionResult AndroidBuildTools,
+    AndroidEmulatorDetectionResult Emulator,
+    AndroidAvdManagerDetectionResult AvdManager,
+    AndroidLicenseDetectionResult AndroidLicenses,
+    AndroidStudioDetectionResult AndroidStudio)
+{
+    public TimeSpan CaptureDuration => CompletedAt - CapturedAt;
+}
+
+public interface IEnvironmentSnapshotService
+{
+    Task<EnvironmentSnapshot> CaptureAsync(CancellationToken cancellationToken = default);
+}
