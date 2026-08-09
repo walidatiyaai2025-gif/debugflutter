a87d4ceb6ea622426cdbf92b6778ba95e7458244
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-09 08:32:01 +0300

    [FBD-001] create .NET 8 solution skeleton

---
+++ src/FlutterBuildDoctor.App/App.xaml.cs
@@ -0,0 +1,5 @@
+namespace FlutterBuildDoctor.App;
+
+public partial class App : System.Windows.Application
+{
+}
---
+++ src/FlutterBuildDoctor.App/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+namespace FlutterBuildDoctor.App;
+
+public partial class MainWindow : System.Windows.Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
