34b95b78ac31f2165835c85b5345b6fc898b6229
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-09 08:02:03 +0300

    FBD-002: add presentation composition root

---
+++ src/FlutterBuildDoctor.App/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+using FlutterBuildDoctor.App.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlutterBuildDoctor.App.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddFlutterBuildDoctorPresentation(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddSingleton<MainWindowViewModel>();
+        services.AddSingleton<MainWindow>();
+
+        return services;
+    }
+}
